fed80fe79c263b59701a8645e9dfa190759c6448
yonghao <12345678>
2020-03-10 15:42:58 +0800

    富文本编辑器

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -16,7 +16,11 @@
         <el-input v-model="model.title"></el-input>
       </el-form-item>
       <el-form-item label="详情">
-        <el-input v-model="model.body"></el-input>
+        <vue-editor
+          v-model="model.body"
+          useCustomImageHandler
+          @image-added="handleImageAdded"
+        ></vue-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -26,7 +30,9 @@
 </template>
 
 <script>
+import { VueEditor } from 'vue2-editor'
 export default {
+  components: { VueEditor },
   props: {
     id: {}
   },
@@ -37,6 +43,14 @@ export default {
     }
   },
   methods: {
+    async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
+      const formData = new FormData()
+      formData.append('file', file)
+
+      const res = await this.$http.post('upload', formData)
+      Editor.insertEmbed(cursorLocation, 'image', res.data.url)
+      resetUploader()
+    },
     async save() {
       let res
       if (this.id) {
