9d732e692e99f84f99d3516b43f554e4d92879c4
yonghao <12345678>
2020-03-13 18:04:28 +0800

    卡片组件提交

--- web/src/main.js
+++ web/src/main.js
@@ -11,6 +11,13 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
+import Card from './components/Card.vue'
+Vue.component('m-card', Card)
+
+import ListCard from './components/ListCard.vue'
+Vue.component('m-list-card', ListCard)
+
+
 new Vue({
   router,
   render: h => h(App)
